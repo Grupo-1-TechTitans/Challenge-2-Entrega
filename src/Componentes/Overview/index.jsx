26722cd9611bdc61bb460a87564070566edbbaf6
Johnvitordev <joaovitor.dev14@gmail.com>
2024-08-20 19:57:45 -0300

    adicionando o video card no sobreProjeto

---
+++ src/Componentes/Overview/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import VideoCard from '../VideoCard/index.jsx';
+import './Overview.css';
+
+
+
+const Overview = () => {
+  return (
+    <div className="overview">
+      <h2><strong>Overview do Project</strong></h2>
+      <div className="video-grid">
+        <VideoCard className="title" title="Video Pitch 1" videoId="-R0UYHS8A_A" />
+        <VideoCard className={"title"} title="Video Pitch 2" videoId="-Pkj1qIcK2Y" />{/* aqui adiciona o ID do video no yt para poder assitir na pagina */}
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
+ 
