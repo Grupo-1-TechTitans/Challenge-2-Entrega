1479c570a8d9e9412eed992b02222273569f509e
Johnvitordev <joaovitor.dev14@gmail.com>
2024-08-28 22:12:51 -0300

    adicionando o componente LoginAzul

---
+++ src/Componentes/LoginAzul/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css"
+
+function LoginAzul() {
+  return (
+    <div className="azul-container">
+      <div className="circ1" />
+      <div className="circ2" />
+      <h1>Tech Titans</h1>
+      <p>Tecnologia com foco em eficiência</p>
+      <div className="botaoMais">
+        <a href="/">
+          <p>Mais</p>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default LoginAzul;
